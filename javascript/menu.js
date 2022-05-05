2a1128465b9b8080816e98d75cd12ce7ddceb2d4
Yelcde <johnnercastrojy@gmail.com>
2022-05-05 11:29:44 -0300

    inicio do menu responsivo

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,11 +1,3 @@
-var mexer = document.getElementById('descer')
-mexer.addEventListener('mouseenter', entrar)
-mexer.addEventListener('mouseout', sair)
-
-function entrar(){
-    mexer.style.paddingTop = '2000px'
-}
-
-function sair(){
-    mexer.style.paddingBottom = "20px"
+function openNav(){
+    alert('Teste')
 }
